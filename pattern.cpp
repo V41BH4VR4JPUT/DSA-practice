c899093aa9f8d734226ec16fae2434938be382c3
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-18 01:25:56 +0530

    Patterns

--- pattern.cpp
+++ pattern.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 
 int main(){
-    cout<<"HELLO DSA!!!"<<endl;
+    int n  ;
+    cin>>n;
+    // pattern 1
+    // int i =1;
+     
+    // while(i<=n){
+    //     int j = 1;
+    //     while(j<=n){
+    //         cout<<i<<" ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+        
+        
+    // }
+    // pattern 2
+    int i =0;
+     
+    while(i<n){
+        int j = 0;
+        while(j<n){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+        
+    }
+    
     return 0;
 }
