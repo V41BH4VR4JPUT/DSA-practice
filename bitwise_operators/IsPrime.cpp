6239cb36889c06dcf836f4e71f9149133bb90df9
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-20 02:09:01 +0530

    Checking Prime Number

---
+++ bitwise_operators/IsPrime.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int main(){
+        int n;
+        cin >> n;
+
+        bool isPrime = true;
+        for(int i = 2; i<n; i++){
+            if(n%i == 0){
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime){
+            cout << "Prime";
+        }else{
+            cout << "Not Prime";
+        }
+        return 0;
+    }
