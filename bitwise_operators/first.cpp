e3f85bfaa1f70d86b0f1d255f0e29b5c677fcdae
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-20 01:29:14 +0530

    Bitwise operaters

---
+++ bitwise_operators/first.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int a = 4;
+    // int b = 6;
+
+    //performing operations on a and b
+    // cout << (a & b) << endl;
+    // cout << (a | b) << endl;
+    // cout << (a ^ b) << endl;    
+    // cout << (~a) << (~b) <<endl;
+
+    // cout<<(17>>1)<<endl;
+    // cout<<(19<<1)<<endl;
+
+    int A , B = 1;
+    A = 10;
+    if(++A){
+        cout<<B;
+    }else{
+        cout<<++B;
+    }
+    return 0;
+}
