59dfd5af18ddf335ac522d37de68d9c69b50744f
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-22 23:44:03 +0530

    Leetcode Solutions

---
+++ LeetCode Questions/Q1281.cpp
@@ -0,0 +1,13 @@
+// Question Nunmber 1281
+// Question Name: Subtract the Product and Sum of Digits of an Integer
+// solution of the question
+
+ /*
+   while(n != 0){
+   int digit = n % 10;
+   sum += digit;
+   product *= digit;
+   n /= 10;
+   }
+    return product - sum;
+ */
---
+++ LeetCode Questions/Q191.cpp
@@ -0,0 +1,12 @@
+// Question Number 191
+// Question Name: Number of 1 Bits
+// Solution of the question
+
+/*
+    int count = 0;
+    while(n != 0){
+      if(n & 1) count++;
+      n = n >> 1;
+    }    
+    return count;
+*/
