f4152de7b38f60645cb4368d940beaea9bbcbd2e
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-25 23:31:59 +0530

    Source Code

---
+++ Arrays/array.cpp
@@ -0,0 +1,11 @@
+// initalizing an array with only one value 
+
+#include<bits/stdc++.h>  
+using namespace std;
+int main(){
+    int arr[5] = {1};
+    for(int i = 0; i < 5; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
