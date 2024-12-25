d0affaac5b6b27c2d31cca91e38901e038b0b3cb
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-25 23:59:15 +0530

    Source Code

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -3,8 +3,11 @@
 #include<bits/stdc++.h>  
 using namespace std;
 int main(){
-    int arr[5] = {1};
-    for(int i = 0; i < 5; i++){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = 1;
         cout<<arr[i]<<" ";
     }
     return 0;
---
+++ Arrays/array2.cpp
@@ -0,0 +1,35 @@
+// printing max and min value from the array
+#include<bits/stdc++.h> 
+using namespace std;
+int printMin(int arr[], int n){
+    int min = INT_MAX;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+int printMax(int arr[], int n){
+    int max = INT_MIN;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int main(){
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+
+    int arr[100];
+
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+    cout<<"The minimum value in the array is: "<<printMin(arr, size)<<endl;
+    cout<<"The maximum value in the array is: "<<printMax(arr, size)<<endl;
+    return 0;
+}
