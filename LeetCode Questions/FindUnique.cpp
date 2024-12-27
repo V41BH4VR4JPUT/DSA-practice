2b89b5d8823bdcd3f87068a2882fa390faedbfc3
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-27 09:36:32 +0530

    Source Code

---
+++ LeetCode Questions/FindUnique.cpp
@@ -0,0 +1,10 @@
+// question from CodeStudio
+// Find the unique element in an array where all elements are repeated twice except one element.
+
+// int findUnique(int arr[], int size) {
+//     int result = 0;
+//     for(int i = 0; i < size; i++){
+//         result = result ^ arr[i];
+//     }
+//     return result;
+// }
