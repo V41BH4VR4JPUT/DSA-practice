bbc27a3aacc28ae2b3aed1ac64b9955a5df95c56
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2024-12-28 23:41:42 +0530

    Source Code

---
+++ LeetCode Questions/Q1207.cpp
@@ -0,0 +1,3 @@
+// Question Number = 1207
+// Question Title = Unique Number of Occurrences
+// solution of Question 1207
---
+++ LeetCode Questions/Q442.cpp
@@ -0,0 +1,12 @@
+// Question Number 442
+// Question Title  Find All Duplicates in an Array
+// Solution of the question
+// optimised solution
+if(nums.empty())return {};
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]));
+            nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+        }
+        return ans;
