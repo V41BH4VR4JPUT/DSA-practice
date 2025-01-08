a2fe6226f6e3b88888e64ee9f345d7802e251271
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-01-08 23:02:40 +0530

    Source Code

---
+++ Arrays/Practice Questions/2.cpp
@@ -0,0 +1,28 @@
+//Implement a function to reverse an array in place.
+#include <bits/stdc++.h>
+using namespace std;
+void ReverseArray(int arr[], int size){
+    int arr2[size];
+    for(int i = 0, j = size -1; i<size && j >=0; i++, j--){
+        arr2[i] = arr[j];
+    }
+    for(int i = 0; i < size; i++){
+        cout<<arr2[i]<<" ";
+    }
+}
+int main(){
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+    cout<<"Reversed array is: ";
+    ReverseArray(arr, size);
+    // for(int i = 0; i < size; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    return 0;
+}
