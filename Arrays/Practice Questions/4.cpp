a2d68e533585b1991eaafc955eb09b8206aad6b1
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-01-09 21:33:53 +0530

    Source Code

---
+++ Arrays/Practice Questions/4.cpp
@@ -0,0 +1,20 @@
+//Rotate an array to the right by k steps.
+#include <bits/stdc++.h>
+using namespace std;
+void rotatory(int arr[], int n, int k){
+    
+}
+int main(){
+    int n ;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i< n; i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter the number of steps: ";
+    cin>>k;
+
+    rotatory(arr, n, k);
+}
---
+++ Searching algorithms/Binarysearch.cpp
@@ -0,0 +1,27 @@
+// Binary Search Time Complexity : O(logn)
+#include<bits/stdc++.h>
+using namespace std;
+int BinarySearch(int arr[], int n, int k){
+    int start = 0;
+    int end = n - 1;
+    int mid = (start + end) / 2;
+    while(start <= end){
+        if(arr[mid] == k){
+            return true;
+        }else if(arr[mid] < k){
+            start = mid + 1;
+        }else{
+            end  = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return false;
+}
+int main(){
+    int even[6] = {2 , 4 , 6, 8 , 10 , 12};
+    int odd[5] = {1 , 3 , 5 , 7 , 9};
+    int k = 8;
+    cout<<BinarySearch(even , 6 , 8)<<endl;
+    cout<<BinarySearch(odd , 5 , 8);
+    return 0;
+}
