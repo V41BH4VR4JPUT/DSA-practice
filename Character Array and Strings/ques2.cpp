d9ebe6c89f81087256a696827abd40e0c6ef1e12
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-01-28 00:15:19 +0530

    Source Code

---
+++ Character Array and Strings/ques2.cpp
@@ -0,0 +1,19 @@
+// find the length of string
+
+#include<bits/stdc++.h>
+using namespace std;
+int LengthOFstring(char name[]){
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++){
+        count++; 
+    }
+    return count;
+}
+int main(){
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+    
+    cout<<"Hello "<<name<<endl;
+    cout<<"Length of name: "<<LengthOFstring(name)<<endl;
+}
