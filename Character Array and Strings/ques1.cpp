d7d34d4809317bd822539aec7ec16f7009513b3a
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-01-28 00:11:27 +0530

    Source Code

---
+++ Character Array and Strings/ques1.cpp
@@ -0,0 +1,11 @@
+// character array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    return 0;
+}
--- practice questions/SumoftwoArrays.cpp
+++ practice questions/SumoftwoArrays.cpp
@@ -13,47 +13,6 @@ ans  = [6,9,1,3,4]
 using namespace std;
 int main(){
     
-    int n1;
-    cin>>n1;
-    int arr1[n1];
-    for(int i=0;i<n1;i++){
-        cin>>arr1[i];
-    }
-    int n2;
-    cin>>n2;
-    int arr2[n2];
-    for(int i=0;i<n2;i++){
-        cin>>arr2[i];
-    }
-    int sum = 0;
-    int carry = 0;
-    int i = n1-1;
-    int j = n2-1;
-    while(i>=0 && j>=0){
-        sum = arr1[i] + arr2[j] + carry;
-        carry = sum/10;
-        sum = sum%10;
-        cout<<sum<<endl;
-        i--;
-        j--;
-    }
-    while(i>=0){
-        sum = arr1[i] + carry;
-        carry = sum/10;
-        sum = sum%10;
-        cout<<sum<<endl;
-        i--;
-    }
-    while(j>=0){
-        sum = arr2[j] + carry;
-        carry = sum/10;
-        sum = sum%10;
-        cout<<sum<<endl;
-        j--;
-    }
-    if(carry!=0){
-        cout<<carry<<endl;
-    }
     
     return 0;
 }
