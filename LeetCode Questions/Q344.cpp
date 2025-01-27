e6b40c65166fbb286f390cc6c6b75236acb5c283
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-01-28 00:25:39 +0530

    Source Code

---
+++ LeetCode Questions/Q344.cpp
@@ -0,0 +1,18 @@
+// Leetcode question 344
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int start = 0;
+        int end = s.size() - 1;
+        while (start < end)
+        {
+            swap(s[start], s[end]);
+            start++;
+            end--;
+        }
+    }
+};
