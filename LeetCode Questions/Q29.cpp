c395a70232a5d60a10ed1d031ecde76a21685a5e
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-02-26 21:12:19 +0530

    Source Code

--- LeetCode Questions/Q29.cpp
+++ LeetCode Questions/Q29.cpp
@@ -8,6 +8,26 @@ class Solution
 public:
     int divide(int dividend, int divisor)
     {
-        
+        if (dividend == INT_MIN && divisor == -1)
+        {
+            return INT_MAX;
+        }
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        long long absDividend = abs((long long)dividend);
+        long long absDivisor = abs((long long)divisor);
+
+        long long Quotient = 0;
+        while (absDividend >= absDivisor)
+        {
+            long long tempDivisor = absDivisor, multiple = 1;
+            while (absDividend >= (tempDivisor << 1))
+            {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+            absDividend -= tempDivisor;
+            quotient += multiple;
+        }
+        return sign * quotient;
     }
 };
