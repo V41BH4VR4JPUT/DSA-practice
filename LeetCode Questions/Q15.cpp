a1d504f4fce415bfd50d534085415936a4d0c837
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-03-03 12:45:54 +0530

    Source Code

---
+++ LeetCode Questions/Q15.cpp
@@ -0,0 +1,42 @@
+// Leetcode q15 : 3 sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(j<k && nums[k]==nums[k-1]) k--;
+                    j++;
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// int main(){
+//     vector<int> nums = {-1,0,1,2,-1,-4};
+//     sort(nums.begin() , nums.end());
+//     for(auto num : nums){
+//         cout<<num<<" ";
+//     }
+// }
