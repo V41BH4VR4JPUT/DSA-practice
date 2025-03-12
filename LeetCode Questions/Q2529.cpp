5e0939ab7156c3be52ddeba51b05d36f92aa034c
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-03-13 00:02:33 +0530

    Source code

---
+++ LeetCode Questions/Q2529.cpp
@@ -0,0 +1,13 @@
+// Leetcode Question 2529 : maximum count of positive integer and negative integer
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) pos++;
+            else if (nums[i] < 0) neg++;
+        }
+        return max(pos, neg);
+    }
+};
