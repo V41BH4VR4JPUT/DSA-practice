bccb463b35bafdf13b8181c5d44eb523b982de44
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-03-26 00:51:39 +0530

    Source Code

---
+++ Double pointers/PointersandFunction.cpp
@@ -0,0 +1,28 @@
+// double pointers and functions 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int **p)
+{
+    **p = **p + 1; // changes the value of i
+    *p = *p + 1; // changes the value of p
+   // *p = *p + 1; // does not change the value of **p
+    // p = p + 1; // does not change the value of **p
+    // cout << **p << endl;
+}
+int main(){
+    int i = 10;
+    int *p = &i;
+    int **q = &p;
+    cout<<"before"<<endl;
+    cout << i <<endl;
+    cout << *p << endl;
+    cout << **q << endl;
+    fun(q);
+    cout <<"after" << endl;
+    cout << i <<endl;
+    cout << *p << endl;
+    cout << **q << endl;
+    return 0;
+}
