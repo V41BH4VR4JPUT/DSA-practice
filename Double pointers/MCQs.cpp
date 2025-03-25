f2393a0fb366e8b5dc8bef4575a18b4b5c93e175
V41BH4VR4JPUT <2812vaibhav@gmail.com>
2025-03-26 01:04:38 +0530

    Source Code

---
+++ Double pointers/MCQs.cpp
@@ -0,0 +1,23 @@
+// MCQs for pointers 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Ques 1:
+    // int first = 8;
+    // int second  = 18;
+    // int *p = &second;
+    // *p = 9;
+    // cout<<first<<" "<<second<<endl;
+
+    // Ques 2:
+    /*
+    int first = 6;
+    int *p = &first;
+    int *q = p;
+    (*q)++;
+    cout<<first<<endl; 
+    */
+    return 0;
+}
