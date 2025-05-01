1d368743df46b332989b10c0e84aeab5907954f0
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-05-01 20:56:01 +0530

    source code

--- STL/Algorithms/algos.cpp
+++ STL/Algorithms/algos.cpp
@@ -2,7 +2,6 @@
 #include <bits/stdc++.h>
 #include <algorithm>
 using namespace std;
-
 int main(){
     vector<int> v={1,2,3,4,5};
     for (auto i : v) cout<<i<<" ";
@@ -29,5 +28,4 @@ int main(){
     sort(v.begin(),v.end());
     for(auto i : v) cout<<i<<" ";
     cout<<endl;
-
 }
